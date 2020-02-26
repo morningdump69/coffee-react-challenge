6f94bcc268e7dcc639fabc9cddeddf178fd02b4b
rossburrows <rossburrows96@gmail.com>
2020-02-26 14:33:27 +0000

    Updates

---
+++ precache-manifest.115da0c7766891fda1818c140faa16a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ff4b842b9722c5479e1b53e47237ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a41dcda7f1ab9475306",
+    "url": "/static/css/main.2edfcce9.chunk.css"
+  },
+  {
+    "revision": "778bbbe8e49754c67b35",
+    "url": "/static/js/2.fd87875a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fd87875a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a41dcda7f1ab9475306",
+    "url": "/static/js/main.bdc49ecb.chunk.js"
+  },
+  {
+    "revision": "72ad7fc1b09692773250",
+    "url": "/static/js/runtime-main.6799207f.js"
+  }
+]);
